d90304aaa90209f1043e630382a7ed731330d971
nirbhay001 <nirbhaypandey019@gmail.com>
2024-02-14 21:36:13 +0530

    second commit

---
+++ app/commonJson/navbarLiks.js
@@ -0,0 +1,20 @@
+const links = [
+    {
+        href: '/saran',
+        name: "Saran"
+    },
+    {
+        href: '/bihar',
+        name: "Bihar"
+    },
+    {
+        href: '/national',
+        name: "National"
+    },
+    {
+        href: '/sport',
+        name: "Sport"
+    }
+];
+
+export default links;
---
+++ app/saran/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Header'
+
+const page = () => {
+    return (
+        <div>
+            <Navbar />
+            <div>Saran page</div>
+        </div>
+    )
+}
+
+export default page
